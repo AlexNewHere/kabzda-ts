06d7be4d7dd92cdf2c04ef240e53ce46a3550b54
Alex <tendren3310@googlemail.com>
2022-05-24 19:52:20 +0300

    advanced useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default {
+    title: 'useState ',
+}
+
+function genereateRandomNumber() {
+    console.log('genereateRandomNumber');
+    return Math.floor(Math.random() * 10);
+}
+
+
+
+export const Example = () => {
+    console.log('Example render')
+
+    const [count, setCount] = React.useState(genereateRandomNumber);
+
+
+    const [change, setChange] = React.useState(genereateRandomNumber);
+
+    const changer = (state: number) => {
+        return state + 1;
+    }
+
+    return <>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <div>{count}</div>
+
+ <button onClick={() => setChange(changer)}>+</button>
+        <div>{change}</div>
+
+
+    </>
+}
