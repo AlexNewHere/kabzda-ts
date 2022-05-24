e4b924455f88e8c4dd51c5b6b47da0a28260d132
Alex <tendren3310@googlemail.com>
2022-05-24 21:06:56 +0300

    useEffect setTimeout

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -14,11 +14,39 @@ export const Example = () => {
         console.log('useEffect every render')
     })
     useEffect(() => {
-        console.log('useEffect only first render 9componentDidMount')
+        console.log('useEffect only first render componentDidMount')
     }, [])
     useEffect(() => {
         console.log('useEffect first render and every counter render')
     }, [count])
+
+    return <>
+
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <div>{count}</div>
+
+
+    </>
+}
+
+
+export const SetTimeoutExample = () => {
+    console.log('SetTimeoutExample')
+
+    const [count, setCount] = React.useState(1);
+
+    useEffect(() => {
+        console.log('useEffect every render')
+        setTimeout(() => {
+            console.log('setTimeout render later 1s')
+        }, 1000)
+
+        // setInterval(() => {
+        //     console.log('setInterval render every 1s')
+        // }, 1000)
+
+    }, [])
+
 
     return <>
 
